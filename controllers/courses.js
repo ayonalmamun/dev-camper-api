97bb153231ee8bd543560bac772bcf5f1dccfa95
unknown <ayonalmamun@gmail.com>
2021-11-02 00:44:47 +0600

    populate bootcamp in courses, virtually populate courses in bootcamp, cascade delete courses when bootcamp is deleted

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Course = require('../models/Course');
+const Bootcamp = require('../models/Bootcamp');
 
 // @desc Get courses
 // @route GET /api/v1/courses
@@ -12,11 +13,18 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     if(req.params.bootcampId) {
         query = Course.find({ bootcamp: req.params.bootcampId });
     } else {
-        query = Course.find();
+        query = Course.find().populate({
+            path: 'bootcamp',
+            select: 'name description'
+        });
     }
 
-    const courses = await query;
+    // check if the bootcamp exist in db
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+    if(!bootcamp) return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.bootcampId}`, 404));
 
+    const courses = await query;
+   
     res.status(200).json({success: true, count: courses.length, data: courses});
 });
     
